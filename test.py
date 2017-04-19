a92d29d1dba920112697a279993a073249c52ef5
Daniel <li.daniel@berkeley.edu>
2017-04-18 21:06:01 -0700

    a

--- test.py
+++ test.py
@@ -3,3 +3,9 @@ import requests, json
 import urllib2
 
 access_token = "EAACEdEose0cBAF4DW01hVAasw15aIo9RTMRl4vpKutf2twIygd3eTmVbyqEXC1vBbBBWKGK2pkcJH7iHcPPXtHDbAUE4LHVlnZCbp9PgBhK7sZAwSZCdb4UPZAvFHqMo5cBW2I6avyBZAoabXeps02ZB9eDjX4g2UVwRZCIPf1vMvqBwvmDgcCiReU3YqBF3C0ZD"
+
+categories = ["Author", "Editor", "TV Network", "TV Show", "Public Figure", "Journalist", "News Personality",
+					"Lawyer", "Business Person", "Entertainer", "Politician", "Government Official", "Media/News/Publishing",
+					"Industrials", "Education", "Political Organization", "Community/Government", "Political Party"]
+
+
